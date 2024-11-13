87ae377bb5316a8ca29e3b4909cc3ec2660853fb
antnyk <anttipnykanen@gmail.com>
2024-11-13 15:29:49 +0200

    Search results are added to a list. new Component

--- frontend/src/movieSearch.js
+++ frontend/src/movieSearch.js
@@ -1,23 +1,31 @@
 import axios from "axios";
 import { useState } from "react";
+import MovieRow from "./components/MovieRow.js";
 
 function MovieSearch() {
 	const [searchValue, setSearchValue] = useState("");
+	const [movies, setMovies] = useState([]);
 
 	const searchSpecificMovie = () => {
 		axios
 			.post("http://localhost:8001/search", { tmdbQuery: searchValue })
 			.then((response) => {
 				console.log(response.data);
+				// NOTE, ids from tmdb are ALWAYS unique, thats why we can use them as the unique key for list elements
+				const moviesData = response.data.results.map((element) => {
+					return { id: element.id, title: element.title };
+				});
+
+				//console.log(moviesData);
+				setMovies(moviesData);
 			})
 			.catch((error) => console.log(error));
 	};
 
 	return (
+		// change className later
 		<div className="App">
-			<h3>TEST</h3>
-			<button onClick={() => searchSpecificMovie()}>Monkey</button>
-			{/* This is what we actually want... a searchbar*/}
+			<h3>Movie search test</h3>
 			<form>
 				<input
 					type="text"
@@ -27,12 +35,16 @@ function MovieSearch() {
 					onKeyDown={(e) => {
 						if (e.key === "Enter") {
 							e.preventDefault();
-							console.log("enter");
 							searchSpecificMovie();
 						}
 					}}
 				></input>
 			</form>
+			<ul>
+				{movies.map((item) => {
+					return <MovieRow key={item.id} item={item} />;
+				})}
+			</ul>
 		</div>
 	);
 }
