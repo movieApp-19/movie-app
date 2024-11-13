0d33aaf867ac68d9158b6a04ae361c753232c868
antnyk <anttipnykanen@gmail.com>
2024-11-13 18:20:08 +0200

    changed from search to discover -> more filtering options

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -11,22 +11,31 @@ const searchResults = async (req, res, next) => {
 	//const tmdbQuery = "monkey";
 	const tmdbQuery = req.body.tmdbQuery;
 	// other values would be OPTIONAL: year, include_adult, language
-	// req.body.include_adult
-	const include_adult = ""; // Defaults to false
 	// req.body.language
-	const language = ""; // Defaults to en-US
+	const language = ""; // empty by default
 	// req.body.year
 	const year = ""; // example "1999";
+	// req.body.popularity
+	const popularityAorD = "desc"; // Defaults to popularity.desc
 
 	const options = {
 		method: "GET",
-		url: `https://api.themoviedb.org/3/search/movie?query=${tmdbQuery}&include_adult=${include_adult}&language=${language}&page=1&year=${year}`,
+		url: `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&year=${year}&with_original_language=${language}&with_text_query=${tmdbQuery}&page=1&sort_by=popularity.${popularityAorD}`,
 		headers: {
 			accept: "application/json",
 			Authorization: `Bearer ${authorizationKey}`,
 		},
 	};
 
+	//const options = {
+	//	method: "GET",
+	//	url: `https://api.themoviedb.org/3/search/movie?query=${tmdbQuery}&include_adult=${include_adult}&language=${language}&page=1&year=${year}`,
+	//	headers: {
+	//		accept: "application/json",
+	//		Authorization: `Bearer ${authorizationKey}`,
+	//	},
+	//};
+	//
 	try {
 		// query can't be empty!
 		if (!tmdbQuery) {
