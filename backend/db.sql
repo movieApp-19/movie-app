8f888d1e7e1ca3c894de381a31248d3eb9d9b270
sonkajarvi <sonkajarvi@hotmail.com>
2024-11-23 15:10:26 +0200

    simplify db.js

--- backend/db.sql
+++ backend/db.sql
@@ -1,11 +1,10 @@
 drop table if exists Account;
-drop table if exists FangroupMember;
-drop table if exists Fangroup;
+-- drop table if exists FangroupMember;
+-- drop table if exists Fangroup;
 
 create table Account( 
   Account_id serial primary key,
-  Email VARCHAR(255) unique not null,
+  Email VARCHAR(254) unique not null,
   Username VARCHAR(50) unique not null,
-  Password VARCHAR(255) not null -- hashed password
+  Password VARCHAR(60) not null -- hashed password
 );
-
