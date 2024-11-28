281ec1ab78e1c688ace8c718ca5cd37ce6525ddb
antnyk <anttipnykanen@gmail.com>
2024-11-28 14:55:00 +0200

    tests work but browser doesnt

--- backend/tests/User.test.js
+++ backend/tests/User.test.js
@@ -1,6 +1,14 @@
+import fs from "fs";
+import path from "path";
 import request from "supertest"
 import app from "../index.js"
 import { pool }  from "../helpers/db.js"
+
+// We run the db.sql file everytime we use test. This resets the test database.
+const initializeTestDb = () => {
+	const sql = fs.readFileSync(path.resolve(__dirname, "../db.sql"), "utf8");
+	pool.query(sql);
+};
 
 describe('Auth API', () => {
   let server;
@@ -9,6 +17,7 @@ describe('Auth API', () => {
   beforeAll(async () => {
     server = app.listen(4000, () => {
         console.log('Test server is running on http://localhost:4000');
+        initializeTestDb()
     });
 
     // Siivoa tietokanta ennen testejä
@@ -17,7 +26,7 @@ describe('Auth API', () => {
     // Rekisteröi käyttäjä kirjautumistestiä varten
     await request(server)
         .post('/user/register')
-        .send({ username: "testuser", email: 'testuser4@example.com', password: 'Password123' });
+        .send({ username: "testuser", email: 'testuser@mail.com', password: 'Password123' });
   });
 
   // Sulje palvelin ja tietokantayhteys testien jälkeen
@@ -27,62 +36,29 @@ describe('Auth API', () => {
       server.close();
   });
 
-  describe('POST /user/login', () => {
-    it('should return success message and token on valid credentials', async () => {
+  describe("POST /user/login", () => {
+    it("Should be succesful. Returns token", async () => {
         const response = await request(server)
-            .post('/user/login')
-            .send({ username: 'testuser', password: 'Password123' });
+            .post("/user/login")
+            .send({ username: "testuser", password: "Password123" });
 
         expect(response.statusCode).toBe(200);
         //user.account_id, user.username, user.email, token
-        expect(response.body.username).toBe('testuser');
+        expect(response.body.id).toBe(1);
+        expect(response.body.username).toBe("testuser");
+        expect(response.body.email).toBe("testuser@mail.com");
         expect(response.body.token).toBeDefined(); // Tallenna token suojattuja reittejä varten
         token = response.body.token;
     });
-  })
-  /*
-  describe('POST /user/register', () => {
-    it('should register the user and store it in the database', async () => {
-      // Send the registration request
-      const response = await request(app)
-        .post('/user/register')
-        .send({ username: 'p', email: 'p@mail', password: 'Password123' });
-  
-      // Assert the response status and body
-      expect(response.statusCode).toBe(201);
-      expect(response.body.username).toBe('p');
-      expect(response.body.email).toBe('p@mail');
-  
-      // Query the database to check if the user was created
-      const result = await selectUserByEmail('p@mail');
-      expect(result.rowCount).toBeGreaterThan(0); // Ensure user exists in DB
-      expect(result.rows[0].email).toBe('p@mail'); // Verify email matches
+
+    it("Should be fail. Error should be INVALID_CREDENTIALS", async () => {
+        const response = await request(server)
+            .post("/user/login")
+            .send({ username: "testuser", password: "WRONGPASSWORD!" });
+
+        expect(response.statusCode).toBe(401);
+        expect(response.body.error).toBe("Invalid credentials");
+
     });
-  });
-  
-  describe('POST /user/login', () => {
-    it('should return success message and token on valid credentials', async () => {
-      const response = await request(server)
-      .post('/user/login')
-      .send({ username: "p", password: "Password123" });
-      
-      console.log(response.body)
-      expect(response.statusCode).toBe(200);
-      expect(response.body.message).toBe('Login successful!');
-      expect(response.body.token).toBeDefined(); // Tallenna token suojattuja reittejä varten
-      token = response.body.token;
-      }) 
-      });
-      
-      /*
-      describe("DELETE /user/delete-account", ()=>{
-    it("TESTI", async()=>{
-      const response = await request(app)
-        .delete("/user/delete-account")
-        .send({email: 'p@mail'})
-        console.log(response.data)
-        expect(response.statusCode).toBe(201)
-    })
   })
-  */
 })
