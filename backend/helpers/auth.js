78ac54de0a3f45b41bfb82fd8797af191171389d
sonkajarvi <sonkajarvi@hotmail.com>
2024-12-06 03:30:58 +0200

    validate session in auth

--- backend/helpers/auth.js
+++ backend/helpers/auth.js
@@ -1,24 +1,30 @@
 import jwt from "jsonwebtoken";
 import errors from "./errorStrings.js";
+import { selectSession } from "../models/userModel.js";
+import { APIError } from "./APIError.js";
 
 const auth = async (req, res, next) => {
     const token = req.headers.authorization;
     
-    if (!token) {
-        res.status(401).json({ message: errors.AUTH_REQUIRED });
-    } else {
-        try {
-            const p = jwt.verify(token, process.env.JWT_SECRET_KEY);
-            if (!p.username || !p.exp)
-                throw Error;
+    if (!token)
+        return next(new APIError(errors.AUTH_REQUIRED, 401));
 
-            // Pass username and id to next
-            res.locals.username = p.username;
-            res.locals.id = p.id;
-            next();
-        } catch (error) {
-            res.status(403).json({ message: errors.INVALID_CREDENTIALS });
-        }
+    try {
+        const p = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (!p.username || !p.exp)
+            return next(new APIError(errors.AUTH_REQUIRED, 401));
+
+        // Must have a valid session
+        const sessions = (await selectSession(req.headers.authorization))?.rows;
+        if (sessions.length === 0)
+            return next(new APIError(errors.AUTH_REQUIRED, 401));
+
+        // Pass username and id to next
+        res.locals.username = p.username;
+        res.locals.id = p.id;
+        return next();
+    } catch (error) {
+        return next(error);
     }
 }
 
