071dbd958bd14ea278a88771e2b0c336b4ec19e0
sonkajarvi <sonkajarvi@hotmail.com>
2024-12-10 16:51:08 +0200

    include bearer in auth header

--- backend/helpers/auth.js
+++ backend/helpers/auth.js
@@ -4,24 +4,30 @@ import { selectSession } from "../models/userModel.js";
 import { APIError } from "./APIError.js";
 
 const auth = async (req, res, next) => {
-    const token = req.headers.authorization;
-    
-    if (!token)
+    if (!req.headers.authorization)
         return next(new APIError(errors.AUTH_REQUIRED, 401));
 
+    const token = req.headers.authorization?.split(" ")[1];
+    let u = undefined;
+
     try {
-        const p = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        if (!p.username || !p.exp)
+        try {
+            u = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        } catch (error) {
+            return next(new APIError(errors.AUTH_REQUIRED, 401));
+        }
+
+        if (!u.username || !u.id || !u.exp)
             return next(new APIError(errors.AUTH_REQUIRED, 401));
 
         // Must have a valid session
-        const sessions = (await selectSession(req.headers.authorization))?.rows;
+        const sessions = (await selectSession(token))?.rows;
         if (sessions.length === 0)
             return next(new APIError(errors.AUTH_REQUIRED, 401));
 
-        res.locals.username = p.username;
-        res.locals.id = p.id;
-        res.locals.token = req.headers.authorization;
+        res.locals.username = u.username;
+        res.locals.id = u.id;
+        res.locals.token = token;
         return next();
     } catch (error) {
         return next(error);
